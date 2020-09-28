700b047d6bd0493b360183bb17b7e20a2056ace4
anthonygedeon <anthony.gedeonn@gmail.com>
2020-09-27 22:01:40 -0500

    communicate with chrome extension to query all tabs

--- extension/index.js
+++ extension/index.js
@@ -5,6 +5,33 @@ const targetInput = document.querySelector('.current-lookup__input');
 const tabsContainer = document.querySelector('.current-lookup__tabs');
 const clearEscButton = document.querySelector('.current-lookup__clear');
 
+const browser = window.msBrowser || window.browser || window.chrome;
+
+class TabManager {
+
+	constructor() {
+
+		browser.tabs.query({}, (tabs) => {
+			for (let tab of tabs) {
+
+				this.tabs.push(tab);
+
+			}
+		});
+
+		this.tabs = [];
+
+	}
+
+	closeTab(tabId) {
+		return this.tabs.filter(tab => tab.id === tabId);
+	}
+
+}
+
+const tabManager = new TabManager();
+
+
 /**
  *
  */
