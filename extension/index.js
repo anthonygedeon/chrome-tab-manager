0873b17a475051ce2da5436a6f09c73bc8ce7550
anthonygedeon <anthony.gedeonn@gmail.com>
2020-10-05 20:57:00 -0500

    :feature: window html generated by js

--- extension/index.js
+++ extension/index.js
@@ -3,9 +3,13 @@
 import { KEY_NAME } from './util.js';
 import { TabManager } from './TabManager.js';
 import CollectionNavigator from './CollectionNavigator.js';
-import { clearButtonHtml, shortcutEscHtml, tabHtml } from './components.js';
+import {
+	clearButtonHtml,
+	shortcutEscHtml,
+	tabHtml,
+	windowHtml,
+} from './components.js';
 
-const tabListContainer = document.querySelector('.tab-collection');
 
 const browser = window.msBrowser || window.browser || window.chrome;
 
@@ -13,32 +17,54 @@ const manager = new TabManager();
 const tabNavigator = new CollectionNavigator(getTabs());
 
 function getTabs() {
-
 	const allTabs = [];
 
 	manager.tabs((tabs) => {
-
 		for (let tab of tabs) {
 			allTabs.push(tab);
 		}
-
 	});
 
 	return allTabs;
-
 }
 
 function closePopup() {
 	browser.windows.getAll({}, (windows) => {
-
-		const popupId = windows.filter(window => window.type === 'popup')[0].id;
+		const popupId = windows.filter((window) => window.type === 'popup')[0]
+			.id;
 
 		browser.windows.remove(popupId);
-	
+	});
+}
+
+function generateWindowList() {
+	const windowContainer = document.querySelector(
+		'.window-collection__container'
+	);
+
+	if (windowContainer.hasChildNodes()) {
+		Array.from(windowContainer.childNodes).forEach((node) => node.remove());
+	}
+
+	browser.windows.getAll({}, (windows) => {
+
+		for (let window of windows) {
+			if (window.type !== 'popup') {
+				const windowComponent = windowHtml(window);
+
+				windowContainer.insertAdjacentHTML(
+					'beforeend',
+					windowComponent
+				);
+			}
+		}
 	});
 }
 
 function generateTabList() {
+
+	const tabListContainer = document.querySelector('.tab-collection');
+
 	if (tabListContainer.hasChildNodes()) {
 		Array.from(tabListContainer.childNodes).forEach((node) =>
 			node.remove()
@@ -47,7 +73,6 @@ function generateTabList() {
 
 	manager.tabs((tabs) => {
 		for (const tab of tabs) {
-
 			const tabComponent = tabHtml(tab);
 
 			tabListContainer.insertAdjacentHTML('beforeend', tabComponent);
@@ -64,36 +89,41 @@ function generateTabList() {
 				faviconContainer.classList.add('tab__favicon-container--base');
 			}
 		}
+		document.querySelector('.current-lookup__tab-amount').textContent =
+			tabs.length;
 	});
 }
 
 function searchForTab() {
+	const inputFieldText = document.querySelector('.current-lookup__input')
+		.value;
 
-	const inputFieldText = document.querySelector('.current-lookup__input').value;
-
-	const regex = new RegExp(`${inputFieldText}`, 'g')
+	const regex = new RegExp(`${inputFieldText}`, 'g');
 
 	manager.tabs((tabs) => {
-
 		for (let tab of tabs) {
-			tab.url = tab.url.replace(regex, (match) => `<strong>${match}</strong>`)
-			console.log(tabHtml(tab))
+			tab.url = tab.url.replace(
+				regex,
+				(match) => `<strong>${match}</strong>`
+			);
+			console.log(tabHtml(tab));
 		}
-
 	});
-	
-
 }
 
-function renderTabList() {
+function render() {
+
+	generateWindowList();
+
 	generateTabList();
+
 }
 
 function enterHandler() {
 	window.addEventListener('keydown', (event) => {
 		if (event.key === KEY_NAME.enter) {
-			const id = tabNavigator.tabId;
-			manager.goto(id);
+			const tabId = tabNavigator.currentCollectionItem.id;
+			manager.goto(tabId);
 			closePopup();
 		}
 	});
@@ -110,35 +140,6 @@ function tabScrolling() {
 
 	selectedTab.classList.add('tab--active');
 }
-
-document.querySelector('.tab-collection').addEventListener('click', (event) => {
-	if (event.target.classList.contains('tab')) {
-		const id = Number(event.target.dataset.id);
-
-		manager.tabs((tabs) => {
-			const indexOfTab = tabs.findIndex((tab) => tab.id === id);
-
-			tabNavigator.collectionIndex = indexOfTab;
-		});
-
-		manager.goto(id);
-
-		closePopup();
-
-	}
-
-	if (event.target.classList.contains('clear-icon')) {
-		const removeId = Number(
-			event.target.parentElement.parentElement.dataset.id
-		);
-
-		manager.closeTab(removeId);
-
-		renderTabList();
-	}
-});
-
-
 
 window.addEventListener('keydown', (event) => {
 	switch (event.key) {
@@ -161,16 +162,39 @@ window.addEventListener('keydown', (event) => {
 });
 
 window.addEventListener('DOMContentLoaded', () => {
-
-	renderTabList();
+	render();
 
 	document.querySelector('.current-lookup__input').focus();
 
+	document.querySelector('.tab-collection').addEventListener('click', (event) => {
+		if (event.target.classList.contains('tab')) {
+			const id = Number(event.target.dataset.id);
+	
+			manager.tabs((tabs) => {
+				const indexOfTab = tabs.findIndex((tab) => tab.id === id);
+	
+				tabNavigator.collectionIndex = indexOfTab;
+			});
+	
+			manager.goto(id);
+	
+			closePopup();
+		}
+	
+		if (event.target.classList.contains('clear-icon')) {
+			const removeId = Number(
+				event.target.parentElement.parentElement.dataset.id
+			);
+	
+			manager.closeTab(removeId);
+	
+			render();
+		}
+	});
 });
 
-document.querySelector('.current-lookup__input').addEventListener('keydown', (event) => {
-
-
-	searchForTab();
-
-});
+document
+	.querySelector('.current-lookup__input')
+	.addEventListener('keydown', (event) => {
+		searchForTab();
+	});
