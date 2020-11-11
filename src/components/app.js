49ea8b8ec62d56ca4eeff43bbea1aaa42483d452
anthonygedeon <anthony.gedeonn@gmail.com>
2020-11-10 21:09:55 -0600

    created bare bones components, no state just stateless functions for now

--- src/components/app.js
+++ src/components/app.js
@@ -3,8 +3,21 @@ import './app.scss';
 
 const Tab = () => {
     return (        
-        <div>
-            Tab
+        <div class="tab">
+            <div className="container row row--space-between row--center">
+
+                <div className="tab__content">
+                    <div className="tab__favicon-container">
+                        <img src="" alt="Favicon" class="tab__favicon"/>
+                    </div>
+                    <h2 class="tab__title">This is the title of the tab</h2>
+                </div>
+
+                <div className="tab__right">
+                    <h3 class="tab__url"><a href="#" class="tab__link">dummylink.com</a></h3>
+                </div>
+
+            </div>
         </div>
     );
 };
@@ -17,42 +30,42 @@ const TabList = () => {
             <Tab />
             <Tab />
             <Tab />
-            <Tab />
-            <Tab />
-            <Tab />
-            <Tab />
         </div>
     );
 };
 
 const Footer = () => {
     return (
-        <footer>
-            Footer
+        <footer class="footer">
+            <div className="container row row--right">
+                <i class="material-icons md-18 md-light settings-icon">settings</i>
+            </div>
         </footer>
     );
 }
 
 const SearchField = () => {
     return (
-        <div>
-            Search
+        <div class="search-tabs">
+            
+            <div className="container row row--center row--space-between">
+                <div className="search-tabs__search-bar">
+                    <i class="material-icons md-18 md-light search-icon">search</i>
+                    <input class="search-tabs__input" type="text" tabIndex="0" autofocus placeholder="Search for tabs"/>
+                </div>
+    
+                <div class="search-tabs__counter">
+                    <span class="search-tabs__total">5</span>
+                </div>
+            </div>
+            
         </div>
     )
 }
 
-const Header = () => {
-    return (
-        <header>
-            Header
-        </header>
-    );
-};
-
 const App = () => {
     return (
         <Fragment>
-            <Header />
             <SearchField />
             <TabList />
             <Footer />
